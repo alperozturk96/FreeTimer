9d5e6cb23c995ec34e133ed00e27115af4ca4782
Alper Ozturk <alperozturk@lions-macbook.local>
2023-09-27 17:09:14 +0200

    Increase version 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.coolnexttech.freetimer"
         minSdk = 27
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.2"
 
         vectorDrawables {
             useSupportLibrary = true
